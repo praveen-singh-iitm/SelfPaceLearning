567555c0917885ae6453269a4a96d76e0f479e6e
praveen-singh-iitm <praveen.singh.iitm@gmail.com>
2026-01-31 01:50:35 +0530

    adding assignment 6

--- Python/Assignment_6/Calculator_tkinter/calculator.py
+++ Python/Assignment_6/Calculator_tkinter/calculator.py
@@ -8,30 +8,30 @@ first_number = None
 operator = None
 
 def button_click(value):
-    current = display_var.get()
+    current = output_var.get()
     if value == '.' and '.' in current:
         return
-    display_var.set(current + value)
+    output_var.set(current + value)
 
 def button_clear():
     global first_number, operator
-    display_var.set("")
+    output_var.set("")
     first_number = None
     operator = None
 
 def button_operator(op):
     global first_number, operator
     try:
-        first_number = float(display_var.get())
+        first_number = float(output_var.get())
         operator = op
-        display_var.set("")
+        output_var.set("")
     except:
-        display_var.set("Error")
+        output_var.set("Error")
 
 def button_equals():
     global first_number, operator
     try:
-        second_number = float(display_var.get())
+        second_number = float(output_var.get())
         if operator == '+':
             result = first_number + second_number
         elif operator == '-':
@@ -40,17 +40,17 @@ def button_equals():
             result = first_number * second_number
         elif operator == '/':
             if second_number == 0:
-                display_var.set("Error")
+                output_var.set("Error")
                 return
             result = first_number / second_number
         else:
             return
         if result == int(result):
-            display_var.set(str(int(result)))
+            output_var.set(str(int(result)))
         else:
-            display_var.set(str(result))
+            output_var.set(str(result))
     except:
-        display_var.set("Error")
+        output_var.set("Error")
     first_number = None
     operator = None
     
@@ -58,9 +58,9 @@ for i in range(4):
     root.grid_columnconfigure(i, weight=1)
 for i in range(6):
     root.grid_rowconfigure(i, weight=1)
-display_var = tk.StringVar()
-display = tk.Entry(root,font=("Arial", 16),borderwidth=5,justify="right",textvariable=display_var)
-display.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky="nsew")
+output_var = tk.StringVar()
+output = tk.Entry(root,font=("Arial", 16),borderwidth=5,justify="right",textvariable=output_var)
+output.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky="nsew")
 buttons = [('7',1,0), ('8',1,1), ('9',1,2), ('/',1,3),('4',2,0), ('5',2,1), ('6',2,2), ('*',2,3),('1',3,0), ('2',3,1), ('3',3,2), ('-',3,3),('0',4,0), ('.',4,1), ('+',4,2),('C',5,0), ('=',5,1)]
 
 for text, r, c in buttons:
@@ -81,3 +81,4 @@ for text, r, c in buttons:
         btn.grid(row=r, column=c, sticky="nsew", padx=5, pady=5)
 
 root.mainloop()
+
--- Python/Assignment_6/README.md
+++ Python/Assignment_6/README.md
@@ -4,10 +4,6 @@ Course Name:- Python
 
 Assignment Name: Assignmennt 6
 
-In Assignnment_5 we have we have calulator_tkinter folder which have 3 files of calculator code.
+In Assignnment_5 we have we have calulator_tkinter folder which have calculator.py
 
-calculator_part1.py :- It contain GUI structure only but does not contain logic. It shows UI but button will not work.
-
-calculator_part2.py :- It contain GUI structure and numbers and operators will also display but output will not come.
-
-calculator_part3.py :- It will work perfectly as calculator.
+calculator.py :- it contain code for calculator using tkinter.
